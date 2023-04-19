14bbe877266ee18b35dd852dec43f342178bb892
Wilkqs <127328280+Wilkqs@users.noreply.github.com>
2023-04-19 15:13:06 +0000

    ll

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    int high = 5 ;
+    int high =5 ;
     for(int i = 0; i<high; i++) {
       for(int k = 0; k<= i; k++) {
         System.out.print("*");
       
       }
-      for(int j = 1; j < 5-i ;j++){
+      for(int j = 1; j < 5 - i ;j++){
         System.out.print("_");
       }
+      
       System.out.println();
     }
+    
   }
+  
 }
